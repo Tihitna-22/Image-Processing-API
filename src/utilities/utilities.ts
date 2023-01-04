56aafe360c6e9ef766c1ea700b2be105e3e0df0f
Tihitna-22 <tihitna222sisay@gmail.com>
2023-01-04 11:45:11 +0300

    fix style

--- src/utilities/utilities.ts
+++ src/utilities/utilities.ts
@@ -1,45 +1,25 @@
 import sharp from 'sharp'
 import path from 'path'
 
-// interface Query {
-//     filename?: string;
-//     width?: string;
-//     height?: string;
-//   }
+  const processImg =  async (
+    filename: string,
+    width: string,
+    height: string,
+  ): Promise<boolean> => {
+    const pathToImg = path.join(
+        __dirname,
+        `../images/full/${filename}.jpg`
+      )
+      const pathToThumbImg = path.join(
+        __dirname,
+        `../images/thumb/${filename}${width}x${height}.jpg`
+      )
+      
+        await sharp(pathToImg)
+          .resize(Number(width), Number(height))
+          .toFile(pathToThumbImg)
+        // console.log(pathToImg)
+        return true
+  }
 
-const processImg = async (
-  filename: string,
-  width: string,
-  height: string
-  // outputFileName: string
-): Promise<boolean> => {
-  const pathToImg = path.join(__dirname, `../images/full/${filename}.jpg`)
-  const pathToThumbImg = path.join(
-    __dirname,
-    `../images/thumb/${filename}${width}x${height}.jpg`
-  )
-
-  await sharp(pathToImg)
-    .resize(Number(width), Number(height))
-    .toFile(pathToThumbImg)
-  // console.log(pathToImg)
-  return true
-}
-//   const processImg = async (value:Query) : Promise<void> => {
-
-//     const pathToImg = path.join(
-//         __dirname,
-//         `../../images/full/${value.filename}.jpg`
-//       )
-//       const pathToThumbImg = path.join(
-//         __dirname,
-//         `../../images/thumb/${value.filename}${value.width}x${value.height}.jpg`
-//       )
-
-//         await sharp(pathToImg)
-//           .resize(Number(value.width), Number(value.height))
-//           .toFile(pathToThumbImg)
-//         // console.log(pathToImg)
-
-//    }
-export default processImg
+   export default processImg
